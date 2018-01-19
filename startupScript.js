381b323a0a1ae5d823f6694485e31c5a591fd5ac
Pedro Castro <pedrocastro94@gmail.com>
2018-01-19 13:32:02 -0200

    algumas atualizacoes

--- startupScript.js
+++ startupScript.js
@@ -1,5 +1,9 @@
 const myVideo = document.getElementById("video");
 
+let imagem = document.getElementById("turtle");
+
+imagem.addEventListener('click', invertcolor);
+
 function playPause(){
 	if(myVideo.paused){
 		myVideo.play();
@@ -19,4 +23,13 @@ function makeSmall() {
 
 function makeNormal() {
 	myVideo.width = 420;
+}
+
+function invertcolor() {
+    const body = document.getElementById("gorila");
+		if(body.style.filter == "invert(100%)"){
+			body.style.filter = "";
+		} else {
+			body.style.filter = "invert(100%)";
+		};
 }
